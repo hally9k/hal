d087cb5fb930045115a0f6ba584e8737f9ed267e
hally9k <hally9k@gmail.com>
2016-10-02 05:47:14 +1300

    refactored store and root component

--- src/hal/hal.component.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import hal from './hal.component';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<hal />, div);
-});
--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux';
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
+import {configureStore} from './store';
 
-
-import Hal from './hal/hal.container';
+import Root from './components/root/root.component';
 import './index.css';
 
-import reducers from './reducers';
-
-const logger = createLogger();
-
-const store = createStore(reducers, applyMiddleware(thunk, logger));
+const store = configureStore();
 
 ReactDOM.render(
-  <Provider store={store} >
-    <Hal />
-  </Provider>,
+  <Root store={store} />,
   document.getElementById('root')
 );
