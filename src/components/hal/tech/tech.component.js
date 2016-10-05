abfbe2e1d3998168fb36b4569ae52071a9e57092
hally9k <hally9k@gmail.com>
2016-10-06 07:53:09 +1300

    mocked up tech page

--- src/components/hal/tech/tech.component.js
+++ src/components/hal/tech/tech.component.js
@@ -6,8 +6,8 @@ class techComponent extends Component {
   render() {
     return (
       <div>
-        <h1>Things I use...</h1>
-        <div className="tech-logos-container">
+        <h1 className="tech-main-title">Things I use...</h1>
+        {/* <div className="tech-logos-container">
           {
             this.props.state.map((tech) => {
               return <div className="tech-logo-container" key={tech.key}>
@@ -15,7 +15,97 @@ class techComponent extends Component {
                       </div>
             })
           }
+        </div> */}
+        <div className="tech-section">
+          <h3 className="tech-title">Front End</h3>
+          <div className="tech-logos-container">
+            {
+              this.props.state.get('frontend').map((tech) => {
+                return <div className="tech-logo-container" key={tech.key}>
+                          <img className="tech-logo" src={tech.url} alt={tech.key} />
+                        </div>
+              })
+            }
+          </div>
+        	<p>AngularJS & KendoUI Specialist</p>
+        	<p>ReactJS Enthusiast</p>
+          <p>Pure Functional Components with Redux, Please.</p>
+          <p>ES6/7 with Babel, Webpack, Gulp and Yeoman</p>
+          <p>TDD with Karma, Mocha, Chai & Sinon.</p>
+        	<p>Bootstrap User</p>
+        	<p>PostCSS / SCSS / LESS</p>
         </div>
+
+        <div className="tech-section">
+          <h3 className="tech-title">DevOps</h3>
+          <div className="tech-logos-container">
+            {
+              this.props.state.get('devops').map((tech) => {
+                return <div className="tech-logo-container" key={tech.key}>
+                          <img className="tech-logo" src={tech.url} alt={tech.key} />
+                        </div>
+              })
+            }
+          </div>
+        	<p>Continuous Deployment Proponent</p>
+        	<p>Auto Scaling, Immutable Infrastructure for the win</p>
+        	<p>If it moves - automate it.</p>
+        	<p>Git -> Teamcity -> Nuget/Zip -> Octopus Deploy -> AWS - Rinse & repeat</p>
+        </div>
+
+        <div className="tech-section">
+          <h3 className="tech-title">Full Stack</h3>
+          <div className="tech-logos-container">
+            {
+              this.props.state.get('fullstack').map((tech) => {
+                return <div className="tech-logo-container" key={tech.key}>
+                          <img className="tech-logo" src={tech.url} alt={tech.key} />
+                        </div>
+              })
+            }
+          </div>
+          <p>ASP.NET Core MVC 6</p>
+        	<p>MSSQL / MySQL / MariaDB / PostgreSQL</p>
+        	<p>NodeJS, Express & MongoDB – MEAN Stack.</p>
+        	<p>Visual Studio 2015 and Atom</p>
+        </div>
+
+        <div className="tech-section">
+          <h3 className="tech-title">Project</h3>
+          <div className="tech-logos-container">
+            {
+              this.props.state.get('project').map((tech) => {
+                return <div className="tech-logo-container" key={tech.key}>
+                          <img className="tech-logo" src={tech.url} alt={tech.key} />
+                        </div>
+              })
+            }
+          </div>
+        	<p>Scrum with Jira or Kanban with Trello</p>
+        	<p>Time tracking with Toggl</p>
+        </div>
+
+        <div className="tech-section">
+          <h3 className="tech-title">Other</h3>
+          <div className="tech-logos-container">
+            {
+              this.props.state.get('other').map((tech) => {
+                return <div className="tech-logo-container" key={tech.key}>
+                          <img className="tech-logo" src={tech.url} alt={tech.key} />
+                        </div>
+              })
+            }
+          </div>
+        	<p>Qualified Sound Engineer and Audio Geek</p>
+        	<p>Synthesizers distract me</p>
+        	<p>I coach a kid’s basketball team</p>
+        	<p>I like cooking curry and occasionally brewing beer</p>
+          <p>I’ve started contributing to an open source project for creaky old</p>
+          <p>AngularJS folk who want to use Redux with the ui-router:</p>
+          <p>https://github.com/neilff/redux-ui-router</p>
+        </div>
+
+
       </div>
     );
   }
--- src/reducers/tech.reducers.js
+++ src/reducers/tech.reducers.js
@@ -1,101 +1,135 @@
-import {List} from 'immutable';
+import {Map} from 'immutable';
 
 import {actions} from '../actions/tech.actions';
 
-const INITIAL_STATE = new List([
-  {
-    key: 'JS',
-    url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914879/js_lqn0d5.png'
-  },
-  {
-    key: 'angular',
-    url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914878/angular_iimtxg.png'
-  },
-  {
-    key: 'github',
-    url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914879/github_l0fjz4.jpg'
-  },
-  {
-    key: 'redux',
-    url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914880/redux_jwedhc.png'
-  },
-  {
-    key: 'webpack',
-    url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474999559/webpack_an3ush.jpg'
-  },
-  {
-    key: 'kendo',
-    url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914879/kendo_ngglpm.jpg'
-  },
-  {
-    key: 'octopus',
-    url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914880/octopus_uxl3qc.png'
-  },
-  {
-    key: 'atom',
-    url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914878/atom_oftrpi.png'
-  },
-  {
-    key: 'karma',
-    url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914879/karma_otzuq9.png'
-  },
-  {
-    key: 'yeoman',
-    url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474999559/yeoman_pxdiao.png'
-  },
-  {
-    key: 'gulp',
-    url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474999558/gulp_gf6yr4.png'
-  },
-  {
-    key: 'bootstrap',
-    url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914878/bootstrap_xthzqp.png'
-  },
-  {
-    key: 'babel',
-    url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474999558/babel_e3x8wc.png'
-  },
-  {
-    key: 'aws',
-    url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914878/aws_kdkxcb.png'
-  },
-  {
-    key: 'nodejs',
-    url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914879/nodejs_auyd1r.png'
-  },
-  {
-    key: 'aspnetcore',
-    url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474999558/aspnetcore_lkvkmz.png'
-  },
-  {
-    key: 'jira',
-    url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474999558/jira_xksgrh.png'
-  },
-  {
-    key: 'teamcity',
-    url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914880/teamcity_g6xsuu.png'
-  },
-  {
-    key: 'toggl',
-    url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914880/toggl_gak7p4.png'
-  },
-  {
-    key: 'reactjs',
-    url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914880/reactjs_fhmmo5.png'
-  },
-  {
-    key: 'postcss',
-    url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914880/postcss_n6i1d7.png'
-  },
-  {
-    key: 'mongodb',
-    url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474999558/mongodb_uybc5y.png'
-  },
-  {
-    key: 'elm',
-    url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914878/elm_hijrdz.png'
-  }
-]);
+const INITIAL_STATE = new Map({
+  frontend: [
+    {
+      key: 'JS',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914879/js_lqn0d5.png'
+    },
+    {
+      key: 'angular',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914878/angular_iimtxg.png'
+    },
+    {
+      key: 'github',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914879/github_l0fjz4.jpg'
+    },
+    {
+      key: 'redux',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914880/redux_jwedhc.png'
+    },
+    {
+      key: 'webpack',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474999559/webpack_an3ush.jpg'
+    },
+    {
+      key: 'kendo',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914879/kendo_ngglpm.jpg'
+    },
+    {
+      key: 'atom',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914878/atom_oftrpi.png'
+    },
+    {
+      key: 'karma',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914879/karma_otzuq9.png'
+    },
+    {
+      key: 'yeoman',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474999559/yeoman_pxdiao.png'
+    },
+    {
+      key: 'gulp',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474999558/gulp_gf6yr4.png'
+    },
+    {
+      key: 'bootstrap',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914878/bootstrap_xthzqp.png'
+    },
+    {
+      key: 'babel',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474999558/babel_e3x8wc.png'
+    },
+    {
+      key: 'reactjs',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914880/reactjs_fhmmo5.png'
+    },
+    {
+      key: 'postcss',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914880/postcss_n6i1d7.png'
+    }
+  ],
+  devops: [
+    {
+      key: 'git',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1475692483/git_nogbcl.png'
+    },
+    {
+      key: 'teamcity',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914880/teamcity_g6xsuu.png'
+    },
+    {
+      key: 'octopus',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914880/octopus_uxl3qc.png'
+    },
+    {
+      key: 'aws',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914878/aws_kdkxcb.png'
+    }
+  ],
+  fullstack: [
+    {
+      key: 'nodejs',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914879/nodejs_auyd1r.png'
+    },
+    {
+      key: 'aspnetcore',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474999558/aspnetcore_lkvkmz.png'
+    },
+    {
+      key: 'mongodb',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474999558/mongodb_uybc5y.png'
+    },
+    {
+      key: 'visualstudio',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1475692598/vs_qzamy4.png'
+    }
+  ],
+  project: [
+    {
+      key: 'jira',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474999558/jira_xksgrh.png'
+    },
+    {
+      key: 'toggl',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1474914880/toggl_gak7p4.png'
+    },
+    {
+      key: 'trello',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1475692663/trello_ugjh1s.png'
+    }
+  ],
+  other: [
+    {
+      key: 'audio',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1475693238/audio_kpd0kn.png'
+    },
+    {
+      key: 'basketball',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1475693102/basketball_jjauoq.png'
+    },
+    {
+      key: 'curry',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1475693028/curry_npdavs.png'
+    },
+    {
+      key: 'synthesizers',
+      url: 'http://res.cloudinary.com/hally9k/image/upload/c_scale,w_60/v1475692906/synth_a0ahov.png'
+    }
+  ]
+});
 
 export default (state = INITIAL_STATE, action) => {
   switch(action.type) {
