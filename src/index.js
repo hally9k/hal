59d6a4d34d76bfcbb8b312c11cd5429e38bedbbe
hally9k <hally9k@gmail.com>
2017-02-08 10:21:30 +1300

    ...

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {configureStore} from './store';
 import {syncHistoryWithStore} from 'react-router-redux'
-import {browserHistory} from 'react-router'
+import {hashHistory} from 'react-router'
 
 import Root from './components/root/root.component';
 import './index.css';
 
 const store = configureStore();
 
-const history = syncHistoryWithStore(browserHistory, store);
+const history = syncHistoryWithStore(hashHistory, store);
 
 ReactDOM.render(
   <Root store={store} history={history} />,
